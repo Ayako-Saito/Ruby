657080ca3bc71eb9f1369b2e8dbd8989b93c4801
Ayako-Saito <sonaare-urenashi@infoseek.jp>
2019-05-04 01:48:36 +0900

    練習

---
+++ N.rb
@@ -0,0 +1,12 @@
+# 入力した数字の数だけ「*」が出力
+# a = gets.to_i
+# a.times do
+#     print "*"
+# end
+
+# 入力した半角英数字の中に「A」がいくつ入っているか
+# s = gets
+# puts s.count("A")
+
+s = gets
+puts s[n..str.length]
--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-FizzBuzz each文
+# FizzBuzz each文
 (1..100).each do |n|
     if n % 15 == 0
         p 'Fizz Buzz'
@@ -10,6 +10,20 @@ FizzBuzz each文
         p n    
     end
 end   
+
+n = gets.to_i
+(1..n).each do |n|
+    if n % 15 == 0
+        puts "Fizz Buzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    else
+        puts n    
+    end
+end   
+# a >=1 && a <= 100
 
 # for文
 # for n in 1..100
